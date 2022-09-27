3e8cf4f075081ca882fc580153b14f8de02f4012
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2022-09-27 20:24:05 -0300

    agregado menu de usuario y pantalla juego

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
+"user strict"
 document.addEventListener("DOMContentLoaded", () => {
 
     loader();
 
     function loader() {
         setTimeout(function () {
-            document.getElementById('spinner').style.visibility = "hidden";
-            document.getElementById('content').style.visibility = "visible";
+           // document.getElementById('spinner').style.visibility = "hidden";
+            //document.getElementById('content').style.visibility = "visible";
         }, 5000);
     }
 
 
-    const menu = document.querySelector(".menu");
-    const nav = document.querySelector(".nav-list");
 
-    menu.addEventListener("click", function(){
-        nav.classList.toggle('nav-list-active');
+    const burger = document.querySelector(".menu");
+    const close = document.querySelector(".close-icon");
+    const userMenu = document.querySelector(".menu-user");
+    
+    close.addEventListener("click", () =>{
+        userMenu.classList.remove('active');
     })
+   
+    burger.addEventListener("click",() =>{
+        userMenu.classList.toggle('active');
+    })
+
+    
 
 })
 
